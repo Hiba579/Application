6f031e399123dd66e8a880e57ac97bd66327bdcc
Hiba579 <haboiataha@gmail.com>
2023-05-05 13:37:35 +0200

    add applicatinoJon.py

--- test.py
+++ test.py
@@ -1,158 +1,236 @@
 import argparse
-import os
-import random
-import select
-import socket
 import struct
-import sys
 import threading
+import sys
+import os
 import time
+from socket import *
+import headers
 
-# Constants
-HEADER_LENGTH = 12
-MAX_PACKET_SIZE = 1472
-MAX_WINDOW_SIZE = 64
-TIMEOUT = 1
+parser = argparse.ArgumentParser(description="TCP connection that can run in server and client mode")
 
-# Flags
-SYN_FLAG = 0b1000
-ACK_FLAG = 0b0100
-FIN_FLAG = 0b0010
-RST_FLAG = 0b0001
+parser.add_argument('-s', '--server', action='store_true')
+parser.add_argument('-c', '--client', action='store_true')
+parser.add_argument('-b', '--bind', type=str)
+parser.add_argument('-p', '--port', type=int)
+parser.add_argument('-f', '--file', type=str)
+parser.add_argument('-t', '--test_case', type=str, choices=["none","loss","skip_ack"], default="none")
 
+args = parser.parse_args()
 
-def create_packet(seq_num, ack_num, flags, window_size, data):
-    header = struct.pack('!IIHH', seq_num, ack_num, flags, window_size)
-    packet = header + data
-    return packet
+header_format = '!IIIHH'
+buffer = 1472
+buffer1 = 1484
 
+def calculate_checksum(packet):
+    """
+    Calculate the checksum of the packet.
+    """
+    checksum = 0
+    for i in range(0, len(packet), 2):
+        if i == 10:
+            continue
+        checksum += (packet[i] << 8) + packet[i+1]
+    checksum = (checksum >> 16) + (checksum & 0xffff)
+    checksum = ~checksum & 0xffff
+    return checksum
 
-def parse_header(packet):
-    header = packet[:HEADER_LENGTH]
-    seq_num, ack_num, flags, window_size = struct.unpack('!IIHH', header)
-    return seq_num, ack_num, flags, window_size
+def repeat_send(conn, addr, data):
+    """
+    Resend the packet if it is not acknowledged within a timeout period.
+    """
+    print("Packet failed to send... Resending...")
+    while True:
+        try:
+            conn.sendto(data, addr)
+            conn.settimeout(2)
+            conn.recv(1024)
+            print("Received ack")
+            break
+        except timeout:
+            continue
 
+if args.server and args.client:
+    print("Can't run server and client at the same time")
+    sys.exit()
 
-def DRPTClient(ip, port, file, reliable, test_case):
-    pass
+if not args.server and not args.client:
+    print("You have to start in either client or server mode")
+    sys.exit()
 
+if args.server:
+    seq = 0
+    ack = 0
+    addr = (args.bind, args.port)
+    serverSocket = socket(AF_INET, SOCK_DGRAM)
+    serverSocket.bind((args.bind, args.port))
+    f = open(args.file, "wb")
+    expected_seq = 0
+    window_size = 5
+    buffer = [None] * window_size
+    ack_pack = struct.pack(header_format, 0, 0, 4, 0, 0)
 
-class Receiver:
-    def __init__(self, ip_address, port, file_name, reliable_method):
-        self.ip_address = ip_address
-        self.port = port
-        self.file_name = file_name
-        self.reliable_method = reliable_method
-        self.file = None
-        self.next_seq_num = 1
-        self.expected_ack_num = 1
-        self.recv_buffer = []
-        self.recv_buffer_lock = threading.Lock()
-        self.recv_window = MAX_WINDOW_SIZE
-        self.sender_address = None
-        self.sender_window_size = 0
-        self.sender_address_lock = threading.Lock()
-        self.timer = None
-        self.timer_lock = threading.Lock()
+    while True:
+        # Receive a packet
+        data, addr = serverSocket.recvfrom(buffer1)
+        header_msg = data[:12]
+        seq, ack, flags, win, checksum = struct.unpack(header_format, header_msg)
+        syn, ack, fin = headers.parse_flags(flags)
 
-        # Create UDP socket
-        self.socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-        self.socket.bind((ip_address, port))
+        # Check if the packet is corrupted
+        if checksum != calculate_checksum(data):
+            print("Received corrupted packet")
+            continue
 
-    def receive(self):
-        while True:
-            packet, address = self.socket.recvfrom(MAX_PACKET_SIZE)
+        # Send an acknowledgement
+        ack_pack = struct.pack(header_format, 0, seq+1, 4, 0, 0)
+        serverSocket.sendto(ack_pack, addr)
 
-            # Parse header and data from packet
-            seq_num, ack_num, flags, window_size = parse_header(packet)
-            data = packet[HEADER_LENGTH:]
-
-            if flags & RST_FLAG:
-                print('Connection reset by peer')
-                self.close()
+        # Handle the packet based on its sequence number
+        # Write any out-of-order packets in the buffer to the file
+        i = 0
+        while buffer[i] is not None:
+            seq, message_body = buffer[i]
+            if seq == expected_seq:
+                f.write(message_body)
+                expected_seq += 1
+                # Slide the window
+                buffer[i] = None
+                i += 1
+            else:
                 break
 
-            if flags & SYN_FLAG:
-                # New connection request
-                print('Received SYN from', address)
-                self.sender_address = address
-                self.sender_window_size = window_size
-                ack_packet = create_packet(0, seq_num + 1, SYN_FLAG | ACK_FLAG, MAX_WINDOW_SIZE, b'')
-                self.socket.sendto(ack_packet, address)
-                print('Sent SYN-ACK to', address)
-            elif flags & ACK_FLAG:
-                # Handle ACK packet
-                print('Received ACK', ack_num, 'from', address)
-                if ack_num > self.next_seq_num:
-                    print('Unexpected ACK number')
-                    continue
+        # Check if the received packet is the last one
+        if fin == 1:
+            break
 
-                if ack_num == self.next_seq_num:
-                    # Stop timer
-                    with self.timer_lock:
-                        if self.timer is not None:
-                            self.timer.cancel()
-                            self.timer = None
+    else:
+        # Put the packet in the buffer
+        i = (seq - expected_seq) % window_size
+        buffer[i] = (seq, message_body)
 
-                    # Update receive buffer and expected ACK number
-                    with self.recv_buffer_lock:
-                        self.recv_buffer.append((seq_num, data))
-                        self.recv_buffer.sort(key=lambda x: x[0])
-                        while self.recv_buffer and self.recv_buffer[0][0] == self.expected_ack_num:
-                            self.file.write(self.recv_buffer.pop(0)[1])
-                            self.expected_ack_num += 1
-                            self.recv_window += 1
+        # Send an acknowledgement
+    ack_pack = struct.pack(header_format, 0, expected_seq, 4, 0, 0)
+    serverSocket.sendto(ack_pack, addr)
 
-                            # Send ACK for received packet
-                            ack_packet = create_packet(0, ack_num, ACK_FLAG, self.recv_window, b'')
-                            self.socket.sendto(ack_packet, address)
-                            print('Sent ACK for packet', seq_num)
+close_socket(serverSocket, f)
 
-                            # If received packet is the expected packet
-                            if seq_num == self.expected_seq:
-                                self.expected_seq += 1
-                                self.buffer.append(packet[12:])
+if args.client:
+ seq = 0
+ack = 0
+clientSocket = socket(AF_INET, SOCK_DGRAM)
+serverAddress = (args.bind, args.port)
+f = open(args.file, "rb")
+message = f.read(buffer)
+expected_ack = 0
+window_size = 5
+buffer = [None] * window_siz
+# Send the first packet with SYN flag set
+syn_pack = struct.pack(header_format, seq, ack, headers.make_flags(1, 0, 0), window_size, 0)
+clientSocket.sendto(syn_pack, serverAddress)
 
-                                # Write all packets in the buffer that are in order
-                                while self.buffer and self.buffer[0]:
-                                    self.file.write(self.buffer.pop(0))
-                                    self.bytes_received += 1460
+while True:
+    try:
+        # Receive an acknowledgement
+        clientSocket.settimeout(2)
+        ack_pack, serverAddress = clientSocket.recvfrom(buffer)
+        header_msg = ack_pack[:12]
+        seq, ack, flags, win, checksum = struct.unpack(header_format, header_msg)
+        syn, ack, fin = headers.parse_flags(flags)
 
-                            # If received packet is not the expected packet
-                            else:
-                                self.buffer[seq_num - self.expected_seq - 1] = packet[12:]
+        # Check if the acknowledgement is corrupted
+        if checksum != calculate_checksum(ack_pack):
+            print("Received corrupted acknowledgement")
+            continue
 
-                            # If all packets have been received, close the connection and file
-                            if self.bytes_received == self.file_size:
-                                print('Received file successfully')
-                                self.file.close()
-                                self.socket.close()
-                                sys.exit()
+        # Handle the acknowledgement based on its sequence number
+        if ack == expected_ack:
+            # Read the next packet from the file
+            message = f.read(buffer)
+            seq += 1
 
-                             # If packet is not valid, discard it
-                               #  excep  InvalidPacketError:
-                                     #  pass
+            # Check if the packet is the last one
+            if not message:
+                fin = 1
 
-                def run(self):
-                    # Start the receiving loop in a separate thread
-                    thread = threading.Thread(target=self.receive_loop)
-                    thread.start()
+            # Send the packet
+            pack = struct.pack(header_format, seq, expected_ack+1, headers.make_flags(0, 1, fin), window_size, 0)
+            pack += message
+            clientSocket.sendto(pack, serverAddress)
 
-                    # Wait for the thread to finish
-                    thread.join()
+            # Write any out-of-order packets in the buffer to the file
+            i = 0
+            while buffer[i] is not None:
+                seq, message_body = buffer[i]
+                if seq == expected_ack + 1:
+                    f.write(message_body)
+                    expected_ack += 1
+                    # Slide the window
+                    buffer[i] = None
+                    i += 1
+                else:
+                    break
 
-            if __name__ == '__main__':
-                # Parse command line arguments
-                parser = argparse.ArgumentParser(description='DRTP file transfer client')
-                parser.add_argument('-i', '--ip', type=str, required=True, help='server IP address')
-                parser.add_argument('-p', '--port', type=int, required=True, help='server port number')
-                parser.add_argument('-f', '--file', type=str, required=True, help='file to transfer')
-                parser.add_argument('-r', '--reliable', type=str, choices=['gbn', 'sr'], default='gbn',
-                                    help='reliable transmission protocol (default: GBN)')
-                parser.add_argument('-t', '--test-case', type=str, default=None,
-                                    help='test case to simulate packet loss (options: loss)')
-                args = parser.parse_args()
+            # Slide the window
+            expected_ack += 1
 
-                # Create a DRTP client and start the transfer
-                client = DRPTClient(args.ip, args.port, args.file, args.reliable, args.test_case)
-                client.run()
+        else:
+            # Put the acknowledgement in the buffer
+            i = (ack - expected_ack) % window_size
+            buffer[i] = (ack, None)
+
+        # Check if the last packet has been acknowledged
+        if fin == 1 and ack == seq+1:
+            break
+
+    except timeout:
+        # Resend the packet
+        repeat_send(clientSocket, server)
+        # Send an acknowledgement
+        ack_pack = struct.pack(header_format, 0, seq + 1, 4, 0, 0)
+        clientSocket.sendto(ack_pack, serverAddr)
+
+        # Handle the packet based on its sequence number
+        if seq == expected_seq:
+        # Write the packet data to the file
+            message_body = data[12:]
+        f.write(message_body)
+        expected_seq += 1
+
+        # Write any out-of-order packets in the buffer to the file
+        i = 0
+        while buffer[i] is not None:
+            seq, message_body = buffer[i]
+            if seq == expected_seq:
+                f.write(message_body)
+                expected_seq += 1
+                buffer[i] = None
+            i = (i + 1) % window_size
+
+            # Slide the window
+        buffer[(seq - expected_seq) % window_size] = (seq, message_body)
+        expected_ack = expected_seq + 1
+        for i in range(window_size):
+            if buffer[i] is not None and buffer[i][0] == expected_ack:
+                message = buffer[i][1]
+                f.write(message)
+                buffer[i] = None
+                expected_ack += 1
+        ack_pack = struct.pack(header_format, 0, expected_ack, 4, 0, 0)
+        clientSocket.sendto(ack_pack, serverAddr)
+
+    else:
+        # Packet was out of order, buffer it
+        buffer[(seq - expected_seq) % window_size] = (seq, message_body)
+        ack_pack = struct.pack(header_format, 0, expected_ack, 4, 0, 0)
+        clientSocket.sendto(ack_pack, serverAddr)
+
+    # Close the client socket and the file
+    clientSocket.close()
+    f.close()
+    print("File transfer complete")
+    clientSocket.close()
+    sys.exit()
+
+
+
---
+++ try.py
@@ -0,0 +1,201 @@
+import argparse
+import struct
+import threading
+import sys
+import os
+import time
+from socket import *
+
+parser = argparse.ArgumentParser(description="UDP connection that can run in server and client mode")
+
+parser.add_argument('-s', '--server', action='store_true')
+parser.add_argument('-c', '--client', action='store_true')
+parser.add_argument('-b', '--bind', type=str)
+parser.add_argument('-p', '--port', type=int)
+parser.add_argument('-f', '--file', type=str)
+parser.add_argument('-t', '--test_case', type=str, choices=["none", "loss", "skip_ack"], default="none")
+
+args = parser.parse_args()
+
+header_format = '!IIIHH'
+buffer = 1472
+buffer1 = 1484
+
+
+def calculate_checksum(packet):
+    """
+    Calculate the checksum of the packet.
+    """
+    checksum = 0
+    for i in range(0, len(packet), 2):
+        if i == 10:
+            continue
+        checksum += (packet[i] << 8) + packet[i + 1]
+    checksum = (checksum >> 16) + (checksum & 0xffff)
+    checksum = ~checksum & 0xffff
+    return checksum
+
+
+def repeat_send(conn, addr, data):
+    """
+    Resend the packet if it is not acknowledged within a timeout period.
+    """
+    print("Packet failed to send... Resending...")
+    while True:
+        try:
+            conn.sendto(data, addr)
+            conn.settimeout(2)
+            conn.recv(1024)
+            print("Received ack")
+            break
+        except timeout:
+            continue
+
+
+if args.server and args.client:
+    print("Can't run server and client at the same time")
+    sys.exit()
+
+if not args.server and not args.client:
+    print("You have to start in either client or server mode")
+    sys.exit()
+
+if args.server:
+    seq = 0
+    ack = 0
+    addr = (args.bind, args.port)
+    serverSocket = socket(AF_INET, SOCK_DGRAM)
+    serverSocket.bind((args.bind, args.port))
+    f = open(args.file, "wb")
+    expected_seq = 0
+    window_size = 5
+    buffer = [None] * window_size
+    ack_pack = struct.pack(header_format, 0, 0, 4, 0, 0)
+
+    while True:
+        # Receive a packet
+        data, addr = serverSocket.recvfrom(buffer1)
+        header_msg = data[:12]
+        seq, ack, flags, win, checksum = struct.unpack(header_format, header_msg)
+        syn, ack, fin = headers.parse_flags(flags)
+
+        # Check if the packet is corrupted
+        if checksum != calculate_checksum(data):
+            print("Received corrupted packet")
+            continue
+
+        # Send an acknowledgement
+        ack_pack = struct.pack(header_format, 0, seq + 1, 4, 0, 0)
+        serverSocket.sendto(ack_pack, addr)
+
+        # Handle the packet based on its sequence number
+        # Write any out-of-order packets in the buffer to the file
+        i = 0
+        while buffer[i] is not None:
+            seq, message_body = buffer[i]
+            if seq == expected_seq:
+                f.write(message_body)
+                expected_seq += 1
+                # Slide the window
+                buffer[i] = None
+                i += 1
+            else:
+                break
+
+        # Check if the received packet is the last one
+        if fin == 1:
+            break
+        if fin == 1:
+            break
+
+        # Put the packet in the buffer
+        i = (seq - expected_seq) % window_size
+        buffer[i] = (seq, message_body)
+
+        # Send an acknowledgement
+        ack_pack = struct.pack(header_format, 0, expected_seq, 4, 0, 0)
+        serverSocket.sendto(ack_pack, addr)
+
+        serverSocket.close()
+        f.close()
+
+    if args.client:
+        seq = 0
+        ack = 0
+        clientSocket = socket(AF_INET, SOCK_DGRAM)
+        serverAddress = (args.bind, args.port)
+        f = open(args.file, "rb")
+        message = f.read(buffer)
+        expected_ack = 0
+        window_size = 5
+        buffer = [None] * window_size
+        # Send the first packet with SYN flag set
+        syn_pack = struct.pack(header_format, seq, ack, headers.make_flags(1, 0, 0), window_size, 0)
+        clientSocket.sendto(syn_pack, serverAddress)
+
+        while True:
+            try:
+                # Receive an acknowledgement
+                clientSocket.settimeout(2)
+                ack_pack, serverAddress = clientSocket.recvfrom(buffer)
+                header_msg = ack_pack[:12]
+                seq, ack, flags, win, checksum = struct.unpack(header_format, header_msg)
+                syn, ack, fin = headers.parse_flags(flags)
+
+                # Check if the acknowledgement is corrupted
+                if checksum != calculate_checksum(ack_pack):
+                    print("Received corrupted acknowledgement")
+                    continue
+
+                # Handle the acknowledgement based on its sequence number
+                if ack == expected_ack:
+                    # Read the next packet from the file
+                    message = f.read(buffer)
+                    seq += 1
+
+                    # Check if the packet is the last one
+                    if not message:
+                        fin = 1
+
+                    # Send the packet
+                    pack = struct.pack(header_format, seq, expected_ack + 1, headers.make_flags(0, 1, fin), window_size,
+                                       0)
+                    pack += message
+                    clientSocket.sendto(pack, serverAddress)
+
+                    # Write any out-of-order packets in the buffer to the file
+                    i = 0
+                    while buffer[i] is not None:
+                        seq, message_body = buffer[i]
+                        if seq == expected_ack + 1:
+                            f.write(message_body)
+                            expected_ack += 1
+                            # Slide the window
+                            buffer[i] = None
+                            i += 1
+                        else:
+                            break
+
+                    # Slide the window
+                    expected_ack += 1
+
+                else:
+                    # Put the acknowledgement in the buffer
+                    i = (ack - expected_ack) % window_size
+                    buffer[i] = (ack, None)
+
+                # Check if the last packet has been acknowledged
+                if fin == 1 and ack == seq + 1:
+                    break
+
+            except timeout:
+                # Resend the packet
+                repeat_send(clientSocket, serverAddress, pack)
+
+        # Close the client socket and the file
+        clientSocket.close()
+        f.close()
+        print("File transfer complete")
+        sys.exit()
+
+
